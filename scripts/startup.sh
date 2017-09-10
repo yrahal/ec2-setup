1cf7eabee37a7b3f20c2bef047cac0bd5b4e84dd
Youcef Rahal <yrahal@users.noreply.github.com>
2017-09-09 21:41:29 -0400

    startup.sh: moved the vncserver launch up in order to avoid mixed output with the nvidia-docker-plugin launch

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,15 +4,18 @@
 sudo service lightdm stop
 sudo xinit &
 
+# Start the server on port 5901
+/opt/TurboVNC/bin/vncserver
+if [[ $? -ne 0 ]] ; then
+  exit 1
+fi
+
 # This should not be needed!
 sudo nvidia-docker-plugin &
 # TRY:
 # https://github.com/NVIDIA/nvidia-docker/issues/116
 # sudo apt-get purge nvidia-docker
 # sudo rm -r /var/lib/nvidia-docker
-
-# Start the server on port 5901
-/opt/TurboVNC/bin/vncserver
 
 # If you wish to run noVNC
 # /opt/noVNC/utils/launch.sh --vnc localhost:5901
